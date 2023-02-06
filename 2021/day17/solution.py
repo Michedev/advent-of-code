68e0033e0accef4f1623178614baf40d905be23b
mikedev <mik3dev@gmail.com>
2023-02-06 17:40:20 +0100

    fixing

--- 2021/day17/solution.py
+++ 2021/day17/solution.py
@@ -1,7 +1,14 @@
+from collections import namedtuple
+from math import ceil, sqrt
 from typing import Tuple
+
+from numpy import sign
 from template import TemplateSolution
 from path import Path
 import re
+
+x_solution = namedtuple('x_solution', ['x_first_0', 'num_steps', 'always_zero'])
+y_solution = namedtuple('y_solution', ['y_first_0', 'num_steps'])
 
 class Solution(TemplateSolution, day=17, year=2021):
 
@@ -42,7 +49,25 @@ class Solution(TemplateSolution, day=17, year=2021):
             c += i
             i += 1
         return c <= y_b
-        
+    
+    def is_in_bound2(self, y_first_0, y_a, y_b):
+        """
+        Given initial velocity, return if the max y value reached is in the target area
+
+        >>> self.is_in_bound(-2, -5, -10)
+        (True, 2)
+        """
+        y_t = 0
+        t = 0
+        if self.verbose:
+            print('testing y_first_0 =', y_first_0)
+        while sign(y_t) != sign(y_a) or y_t > y_a:
+            y_t = y_first_0 * (t+1) - (t * (t + 1)) / 2
+            t += 1
+            if self.verbose:
+                print('\t', 't', t, 'y_t', y_t)
+        is_in_bound = y_t <= y_b
+        return is_in_bound, t        
 
     def solution1(self, data: Tuple[int, int, int, int]):
         x_a, x_b, y_a, y_b = data
@@ -60,8 +85,47 @@ class Solution(TemplateSolution, day=17, year=2021):
     def solution2(self, data: Tuple[int, int, int, int]):
         x_a, x_b, y_a, y_b = data
         y_max_value = 0
-        counter = 0
-        for y_first_0 in range(1, max(abs(y_a), abs(y_b))):
-            if self.is_in_bound(y_first_0, y_a, y_b):
-                counter += 1
+        counter = (y_b - y_a + 1) * (x_b - x_a + 1)  # obvious solutions, the number of points in the target area
+        # the possible x are all the divisors of x_b - x_a + 1
+        if self.verbose:
+            print('=============')
+            print('obvious solutions')
+            for x in range(x_a, x_b + 1):
+                for y in range(y_a, y_b + 1):
+                    print(f'({x}, {y})')
+            print('=============')
+        x_solutions = set()
+        for d in range(2, x_a):
+            reaches_bound = (d * (d + 1) / 2) >= x_a
+            if reaches_bound:
+                t, x_t, is_in_bound = self.x_is_in_bound(d, x_a, x_b)
+                if is_in_bound:
+                    always_zero = (d * (d+1) / 2 ) <= x_b
+                    x_solutions.add(x_solution(d, t, always_zero))
+        y_solutions = set()
+        for y_first_0 in range(-min(abs(y_a), abs(y_b)), max(abs(y_a), abs(y_b))):
+            in_bound, t = self.is_in_bound2(y_first_0, y_a, y_b)
+            if in_bound: y_solutions.add(y_solution(y_first_0, t))
+        if self.verbose:
+            print('x_solutions', '\n'.join(map(repr, x_solutions)))
+            print('y_solutions', '\n'.join(map(repr, y_solutions)))
+        for x_first_0, x_step, always_zero in x_solutions:
+            for y_first_0, t in y_solutions:
+                if always_zero:
+                    if self.verbose: print(f'solution: ( {x_first_0}, {y_first_0})')
+                    counter += 1  
+                else:
+                    if x_step == t:
+                        if self.verbose: print(f'solution: ( {x_first_0}, {y_first_0})')
+                        counter += 1
         return counter
+
+    def x_is_in_bound(self, x_first_0, x_a, x_b):
+        t = 0
+        x_t = 0
+        while x_t < x_a:
+            x_t = x_t + x_first_0
+            t += 1
+            x_first_0 = max(x_first_0 - 1, 0)
+        is_in_bound = (x_t <= x_b)
+        return t,x_t,is_in_bound
